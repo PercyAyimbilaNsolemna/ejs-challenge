5f3886c9a45976b9b5c4026d0f97d6496b8a5fec
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-05-20 23:55:36 +0000

    Add a get method for the contact route to complete challenge 5.

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ app.get('/about', function(req, res) {
   res.render('about', {aboutContent: aboutContent});
 });
 
+//Creates a get method for the contact route
+app.get('/contact', function(req, res) {
+  res.render('contact', {contactContent: contactContent});
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
