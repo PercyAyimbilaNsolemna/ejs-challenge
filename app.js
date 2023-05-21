8a42bc8274419794f31f64b384232a33257740d9
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-05-21 00:08:43 +0000

    Add a get method for the compose route to complete challenge 7.

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.get('/contact', function(req, res) {
   res.render('contact', {contactContent: contactContent});
 });
 
+//Creates a get method for the compose rouse
+app.get('/compose', function(req, res) {
+  res.render('compose');
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
